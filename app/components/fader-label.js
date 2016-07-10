6da28872364ec7710ae4db8744cfe2d294737c56
Nash <sawmebabe@gmail.com>
2016-07-11 00:37:56 +0200

    Added Faker and Completed Generator with Success Labels on Generator buttons

--- app/components/fader-label.js
+++ app/components/fader-label.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	tagName: 'span',
 	classNames: ['label label-success label-fade'],
-
-	isShowing: true,
+	classNameBindings: ['isShowing:label-show'],
+	isShowing: false,
 	isShowingChanged: Ember.observer('isShowing', function(){
 		Ember.run.later(()=>{
 			this.set('isShowing', false);
--- app/models/library.js
+++ app/models/library.js
@@ -2,6 +2,7 @@ import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { hasMany } from 'ember-data/relationships';
 import Ember from 'ember';
+import Faker from 'faker';
 
 export default Model.extend({
   name: attr('string'),
@@ -11,5 +12,16 @@ export default Model.extend({
   // Associations
   books: hasMany('book'),
   
-  isValid: Ember.computed.notEmpty('name')
+  isValid: Ember.computed.notEmpty('name'),
+  randomize(){
+  	this.set('name', Faker.company.companyName() + ' Library');
+  	this.set('address', this._fullAddress());
+  	this.set('phone', Faker.phone.phoneNumber());
+
+  	return this;
+  },
+
+  _fullAddress(){
+  	return `${Faker.address.streetAddress()}, ${Faker.address.city()}`;
+  }
 });
